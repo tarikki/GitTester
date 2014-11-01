af18411eb624a501710dac380d314d0aa1416c04
extradikke <tariq.alsadoon@gmail.com>
2014-11-01 10:30:45 +0100

    first of branch

--- Tester/Tester.py
+++ Tester/Tester.py
@@ -1,4 +1,4 @@
-__author__ = 'extradikke'
+
 
 listA = [x for x in range (1,10)]
 print(listA)
@@ -6,3 +6,6 @@ print(listA)
 'this is a change we do not want'
 listA.extend([13, 14, 16])
 print(listA)
+
+'This is in branch one'
+'so let\'s save this'
